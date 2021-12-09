fd404e4ccc9396809393c150c91f5e76fe9f4f64
gitterra <80519167+gitterra@users.noreply.github.com>
2021-12-09 09:19:49 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
-SERVER = 'http://127.0.0.1:8000/'              # Адрес сервера
-#SERVER = 'https://aiu-check-homework.herokuapp.com/'
+#SERVER = 'http://127.0.0.1:8000/'              # Адрес сервера
+SERVER = 'https://aiu-check-homework.herokuapp.com/'
 PAGE_CHECK = 'app_check/api-v0/check'          # Страница проверки ответов пользователя
 PAGE_CHECK_UL = 'app_check/api-v0/checkul'          # Страница проверки ответов пользователя
 PAGE_QUESTION = 'app_check/api-v0/questions'   # Страница получения вопросов
